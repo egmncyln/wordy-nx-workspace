4f8386bf510c57eec1e3f16eb05d6243ebc77b60
Egemen Ceylan <egemen.ceylan1@gmail.com>
2022-07-16 01:24:32 +0300

    added docs for utils

--- libs/wordy/src/lib/encryption-utils.ts
+++ libs/wordy/src/lib/encryption-utils.ts
@@ -2,6 +2,20 @@ import * as base64 from "base-64";
 
 import * as utf8 from "utf8";
 
+/**
+ * Encrypts given plainText by using utf8 and base64 packages
+ * 
+ * @export
+ * @param {string} plainText
+ * @return {*}  {string}
+ */
 export const encryptString = (plainText: string): string => base64.encode(utf8.encode(plainText));
 
+/**
+ * Decrypts given encryptedText by using utf8 and base64 packages
+ * 
+ * @export
+ * @param {string} encryptedText
+ * @return {*}  {string}
+ */
 export const decryptString = (encryptedText: string): string => utf8.decode(base64.decode(encryptedText));
--- libs/wordy/src/lib/null-check-utils.ts
+++ libs/wordy/src/lib/null-check-utils.ts
@@ -1,11 +1,56 @@
+/**
+ * Checks whether given object is null
+ *
+ * @export
+ * @template T
+ * @param {T} obj
+ * @return {*}  {boolean}
+ */
 export function isNull<T>(obj: T): boolean { return obj === null; }
 
+/**
+ * Checks whether given object is undefined
+ *
+ * @export
+ * @template T
+ * @param {T} obj
+ * @return {*}  {boolean}
+ */
 export function isUndefined<T>(obj: T): boolean { return obj === undefined; }
 
+/**
+ * Checks whether given object is null or undefined
+ * 
+ * @export
+ * @param {object} obj
+ * @return {*}  {boolean}
+ */
 export const isNullOrUndefined = (obj: object): boolean => isNull(obj) || isUndefined(obj);
 
+/**
+ * Checks whether given string is null, undefined or emtpy
+ * 
+ * @export 
+ * @param {string} str
+ * @return {*}  {boolean}
+ */
 export const isNullOrEmptyString = (str: string): boolean => isNull(str) || isUndefined(str) || str.length === 0;
 
+/**
+ * Checks whether given string is null, undefined, empty or contains empty strings
+ * 
+ * @export
+ * @param {string} str
+ * @return {*}  {boolean}
+ */
 export const isNullOrWhiteSpace = (str: string): boolean => isNull(str) || isUndefined(str) || str.trim().length === 0;
 
+/**
+ * Checks whether given array is null, undefined or empty
+ *
+ * @export
+ * @template T
+ * @param {T[]} arr
+ * @return {*}  {boolean}
+ */
 export function isNullOrEmptyArray<T>(arr: T[]): boolean { return isNull(arr) || isUndefined(arr) || arr.length === 0; }
